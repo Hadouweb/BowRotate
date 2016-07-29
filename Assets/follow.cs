3ea03b0e72b8bebe77131e248971cb811df90ba0
hadouweb <kengo14@hotmail.fr>
2016-07-30 00:32:35 +0200

    init

---
+++ Assets/follow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class follow : MonoBehaviour {
+
+	public GameObject left;
+	public GameObject right;
+
+	void Update() {
+
+		Vector3 deltaPos = left.transform.position - right.transform.position;
+		//transform.rotation = Quaternion.LookRotation(holdControl.transform.position - stringControl.transform.position, holdControl.transform.TransformDirection(Vector3.forward));
+
+		Quaternion rot = Quaternion.LookRotation (deltaPos, left.transform.TransformDirection (Vector3.forward));
+
+		transform.rotation = rot;
+
+	}
+
+}
